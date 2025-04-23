333cc9fa4578afd0e781ece88f30e518f38cdf48
ashrate <kimdorung@gmail.com>
2025-04-23 10:17:43 +0900

    feat:print fizzbuzz
    print 'fizzbuzz' if i is multiple if 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,8 @@
-for i in range(1,16+1):
+for i in range(1,17+1):
+    if %15 ==0:
+        print('fizzbuzz')
     if i%3==0:
         print('fizz')
     else:
         print(f'{i}')
-print('It, works!')
+
